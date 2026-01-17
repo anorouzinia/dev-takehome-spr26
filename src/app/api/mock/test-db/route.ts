520a0b1668361d3ea4e71350dd4f338fc74255f5
anorouzinia <ava.norouzinia@gmail.com>
2026-01-16 23:21:48 -0500

    main requirements

--- src/app/api/mock/test-db/route.ts
+++ src/app/api/mock/test-db/route.ts
@@ -23,6 +23,8 @@ import { NextResponse } from "next/server";
 //   return NextResponse.json(testRequest);
 // }
 
+//smaller pagination size means more pages --> fitting less items per page
+const PAGINATION_PAGE_SIZE = 3;
 
 export async function PUT(req: Request) {
   try {
@@ -63,3 +65,81 @@ export async function PUT(req: Request) {
     );
   }
 }
+
+export async function GET(req: Request) {
+  try {
+    await connectToDatabase();
+
+    const url = new URL(req.url);
+    const statusQuery = url.searchParams.get("status");
+    const pageQuery = url.searchParams.get("page");
+    const page = pageQuery ? parseInt(pageQuery) : 1;
+
+    const filter: any = {};
+    const validStatuses = ["pending", "approved", "completed", "rejected"];
+    if (statusQuery && validStatuses.includes(statusQuery)) {
+      filter.status = statusQuery;
+    }
+
+    const requests = await Request.find(filter)
+      .sort({ createdAt: -1 }) // sort where newest requests come first
+      .skip((page - 1) * PAGINATION_PAGE_SIZE) //skip items from previous pages
+      .limit(PAGINATION_PAGE_SIZE);
+
+    const totalCount = await Request.countDocuments(filter); //count of total items that match the filter
+
+    return NextResponse.json({
+      page,
+      pageSize: PAGINATION_PAGE_SIZE,
+      total: totalCount,
+      data: requests,
+    }, { status: HTTP_STATUS_CODE.OK });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch requests" },
+      { status: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
+
+
+export async function PATCH(req: Request) {
+  try {
+    await connectToDatabase();
+
+    const body = await req.json();
+    const { id, status } = body;
+
+    const validStatuses = ["pending", "approved", "completed", "rejected"];
+    // check validation of input
+    if (!id || !status || !validStatuses.includes(status)) {
+      return NextResponse.json(
+        { error: "Invalid id or status" },
+        { status: HTTP_STATUS_CODE.BAD_REQUEST}
+      );
+    }
+
+    const updatedRequest = await Request.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+
+    if (!updatedRequest) {
+      return NextResponse.json(
+        { error: "Request not found" },
+        { status: HTTP_STATUS_CODE.NOT_FOUND }
+      );
+    }
+
+    return NextResponse.json(updatedRequest, { status: HTTP_STATUS_CODE.OK });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to update request" },
+      { status: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
+
